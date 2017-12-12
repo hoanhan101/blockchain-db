12bc855ab6382d555ece22ad5a5698c6a61e26bf
Hoanh An <hoanhan101@gmail.com>
2017-12-11 22:41:43 -0500

    tested `reset()`

--- test/blockchain_db_test.py
+++ test/blockchain_db_test.py
@@ -14,22 +14,20 @@ from random import randint
 import random
 
 if __name__ == '__main__':
-    blockchain_db = BlockchainDB()
-
+    # blockchain_db = BlockchainDB()
+    # blockchain_db.reset()
 
     """
         Mining blocks
     """
-    number_of_loops = 1010
-    for loop in range(number_of_loops):
-        for transaction in range(randint(1, 50)):
-            blockchain_db.add_transaction(sender=(str(uuid4()).replace('-', '')[:-10]),
-                                          recipient=(str(uuid4()).replace('-', '')[:-10]),
-                                          amount=round(random.uniform(1, 10), 2))
+    # number_of_loops = 1010
+    # for loop in range(number_of_loops):
+    #     for transaction in range(randint(1, 50)):
+    #         blockchain_db.add_transaction(sender=(str(uuid4()).replace('-', '')[:-10]),
+    #                                       recipient=(str(uuid4()).replace('-', '')[:-10]),
+    #                                       amount=round(random.uniform(1, 10), 2))
+    #
+    #     blockchain_db.mine_for_next_block()
 
-        blockchain_db.mine_for_next_block()
 
     # pprint(blockchain_db.get_last_block())
-
-    # for block in blockchain_db.get_top_blocks(state='nonce', number=1):
-    #     pprint(block)
